d55bdfd9408e441c049982c59039bc4723bdb5ea
thom <tom@tksfz.org>
2019-02-25 05:03:25 -0800

    Add burger menu and make it toggleable

--- app/js/src/main/scala/Main.scala
+++ app/js/src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import cats.effect.IO
 import outwatch.dom._
 import outwatch.dom.dsl._
 import monix.execution.Scheduler.Implicits.global
@@ -8,36 +9,48 @@ import outwatch.http.Http.{BodyType, Request, Response}
 import io.circe.generic.auto._
 import io.circe.parser.decode
 import org.tksfz.homely.resources.Resource
+import outwatch.ProHandler
 
 import scala.concurrent.Future
 import scala.scalajs.js
 import scala.scalajs.js.typedarray.ArrayBuffer
 
 object Main {
-  def appContainer(content: Observable[VNode]): VNode = {
-    div(
-      htmlTag("nav")(cls := "navbar has-shadow", role := "navigation", attr("aria-label") := "main navigation",
-        div(cls := "navbar-brand",
-          a(cls := "navbar-item", href := "https://bulma.io",
-            span(cls := "icon", i(cls := "fas fa-home fa-2x")),
-            span(h1(cls := "title is-4", "Homely"))
-          )
-        ),
-        div(cls := "navbar-menu",
-          div(cls := "navbar-end",
-            a(cls := "navbar-item",
-              span(cls := "icon", i(cls := "fas fa-cog")),
-              span("Settings"),
+
+  def appContainer(content: Observable[VNode]): IO[VNode] = {
+    val burgerMenuToggleIO = toggleHandler(false)
+      .map(_.mapObservable(if (_) cls := "is-active" else cls := ""))
+    burgerMenuToggleIO.map { burgerMenuActive =>
+      div(
+        htmlTag("nav")(cls := "navbar has-shadow", role := "navigation", attr("aria-label") := "main navigation",
+          div(cls := "navbar-brand",
+            a(cls := "navbar-item", href := "https://bulma.io",
+              span(cls := "icon", i(cls := "fas fa-home fa-2x")),
+              span(h1(cls := "title is-4", styleAttr := "padding-left: 4px; margin-left: 4px", "Homely"))
+            ),
+            a(role := "button", cls := "navbar-burger burger", attr("aria-label") := "menu", attr("aria-expanded") := "false",
+              data.target := "navmenu", burgerMenuActive: Observable[Attr], onClick() --> burgerMenuActive,
+              span(attr("aria-hidden") := "true"),
+              span(attr("aria-hidden") := "true"),
+              span(attr("aria-hidden") := "true"),
             )
-          )
+          ),
+          div(id := "navmenu", cls := "navbar-menu", burgerMenuActive: Observable[Attr],
+            div(cls := "navbar-end",
+              a(cls := "navbar-item",
+                span(cls := "icon", i(cls := "fas fa-cog")),
+                span("Settings"),
+              )
+            )
+          ),
         ),
-      ),
-      htmlTag("section")(cls := "section",
-        div(cls := "container",
-          div(id := "pageContent", content)
+        htmlTag("section")(cls := "section",
+          div(cls := "container",
+            div(id := "pageContent", content)
+          )
         )
       )
-    )
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -58,7 +71,12 @@ object Main {
     }
 
     //OutWatch.renderReplace("#app", myComponent).unsafeRunSync()
-    OutWatch.renderInto("#app", Main.appContainer(html)).unsafeRunSync()
+    (for {
+      content <- Main.appContainer(html)
+      rendered <- OutWatch.renderInto("#app", content)
+    } yield {
+      rendered
+    }).unsafeRunSync()
   }
 
   private def appCard(imgSrc: String, url: String, name: String) = {
@@ -114,4 +132,8 @@ object Main {
       )
     }.share
 
+  private def toggleHandler(seed: Boolean): IO[ProHandler[Unit, Boolean]] = {
+    Handler.create[Unit]
+      .map(_.transformObservable(_.scan(false) { (f, _) => !f }))
+  }
 }
